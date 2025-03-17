38af7643106765c6fa85bc0b76f781cc0268398e
Gosha <jora.smorodsky@gmail.com>
2025-03-17 19:41:44 +0100

    feat/modal-save

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,24 +1,57 @@
-const modal = document.getElementById('myModal');
+import modalTemplate from '../tamplates/modal.hbs';
+const API_KEY = 'J0g5ARVVXHxk4tqnxMbnlKhBmGVuP7cM';
+const API_URL = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${API_KEY}`;
 
-// Get the button that opens the modal
-const btn = document.getElementById('myBtn');
+document
+  .querySelector('[data-modal-open]')
+  .addEventListener('click', fetchEventData);
+document
+  .querySelector('[data-modal-close]')
+  .addEventListener('click', closeModal);
+document.querySelector('.backdrop').addEventListener('click', onBackdropClick);
 
-// Get the <span> element that closes the modal
-const span = document.getElementsByClassName('close')[0];
+function fetchEventData() {
+  fetch(API_URL)
+    .then(response => response.json())
+    .then(data => {
+      if (data._embedded && data._embedded.events.length > 0) {
+        const eventData = data._embedded.events[1];
+        const eventDetails = {
+          images: eventData.images,
+          info: eventData.info,
+          dates: eventData.dates,
+          timezone: eventData.dates.timezone,
+          _embedded: eventData._embedded,
+          name: eventData.name,
+          priceRanges: eventData.priceRanges,
+          url: eventData.url,
+        };
+        renderModal(eventDetails);
+        openModal();
+      } else {
+        console.error('нічего нема');
+      }
+    })
+    .catch(error => console.error('помивка:', error));
+}
 
-// When the user clicks on the button, open the modal
-btn.onclick = function () {
-  modal.style.display = 'block';
-};
+function renderModal(eventData) {
+  const markup = modalTemplate(eventData);
+  document.querySelector('.modal__main-container').innerHTML = markup;
+}
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = 'none';
-};
+function openModal() {
+  document.querySelector('.backdrop').classList.remove('is-hidden');
+  document.body.style.overflow = 'hidden';
+}
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = 'none';
+function closeModal() {
+  document.querySelector('.backdrop').classList.add('is-hidden');
+  document.body.style.overflow = '';
+}
+
+function onBackdropClick(event) {
+  if (event.target === document.querySelector('.backdrop')) {
+    closeModal();
   }
-};
+}
