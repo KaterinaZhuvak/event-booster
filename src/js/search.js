1f7b0316e15edfa1dcbf88ae308edb8a45c2b52f
Yulchikss <yuliapelia19@gmail.com>
2025-03-19 15:48:50 +0200

    save

--- src/js/search.js
+++ src/js/search.js
@@ -3,17 +3,16 @@ import _ from 'lodash'
 
 import cardstemplate from '../templates/cards.hbs'
 
-
 const cardsList = document.querySelector(".events__list")
-
-
 const dropdown = document.getElementById('js-dropdown')
+const dropdownBtn = document.getElementById('js-dropdownBtn')
 const inputSearch = document.getElementById('js-inputSearch')
 
 const URL = 'https://app.ticketmaster.com/discovery/v2/events.json?'
 const APIKEY = 'WuXWoDJHckwpMkp61QMSCgfgs8UYLwBG'
 
-let currentCountry;
+let currentCountry = '';
+let currentKeyword = '';
 
 console.log(countries);
 
@@ -25,114 +24,78 @@ const renderCountries = () => {
         option.textContent = country.country
         dropdown.appendChild(option) 
     })
-    
 }
 
 renderCountries()
 
-
 const chooseCountry = (e) => {
     if(e.target.closest('.search__country')) {
+        // Set current country code
         currentCountry = e.target.dataset.value
-        // console.log(currentCountry)
+        
+        const selectedCountry = e.target.textContent
+        dropdownBtn.textContent = selectedCountry
+        
+        dropdown.classList.remove('is-show')
     }
-    // console.log(e.target);
-    
 }
 
-
-
-
-
-
-const getCardsByCountry = async () => {
-    console.log(currentCountry);
-
-    // chooseCountry(e)
-      const param = new URLSearchParams({
-        apikey: APIKEY,
-        countryCode: currentCountry || ''
-        // keyword: inputSearch.value || ''
-
-        })
-
+const getEvents = async () => {
     try {
+        const param = new URLSearchParams({
+            apikey: APIKEY,
+            size: 20 // Limit results to 20 events
+        })
+        
+        if (currentCountry) {
+            param.append('countryCode', currentCountry)
+        }
+        
+        if (inputSearch.value) {
+            param.append('keyword', inputSearch.value)
+            currentKeyword = inputSearch.value
+        }
+        
+        cardsList.innerHTML = '<li class="events__loading">Loading events...</li>'
+        
         const response = await fetch(URL + param.toString())
-        const posts = await response.json()
-        console.log(posts);
-
-        const events = posts
-               
-        const markup =  events._embedded.events.map(event => {
+        const data = await response.json()
+        
+        if (!data._embedded || !data._embedded.events || data._embedded.events.length === 0) {
+            cardsList.innerHTML = `<li class="events__no-results">No events found${currentCountry ? ' in selected country' : ''}${currentKeyword ? ' matching "' + currentKeyword + '"' : ''}.</li>`
+            return
+        }
+        
+        const markup = data._embedded.events.map(event => {
             const eventparams = {
-            id: event.id,
-            img:event.images[0].url,
-            date:event.dates.start.localDate,
-            name:event.name,
-            location:event._embedded.venues[0].name
+                id: event.id,
+                img: event.images[0].url,
+                date: event.dates.start.localDate,
+                name: event.name,
+                location: event._embedded.venues[0].name
             }
             return cardstemplate(eventparams)
         })
-                
-        cardsList.innerHTML = markup.join("")
         
+        cardsList.innerHTML = markup.join("")
     }
     catch (err) {
         console.log(err)
+        cardsList.innerHTML = '<li class="events__error">Error loading events. Please try again.</li>'
     }
-    
 }
-
-
-const getCardsByKeyword = async () => {
-    console.log(currentCountry);
-
-    // chooseCountry(e)
-      const param = new URLSearchParams({
-        apikey: APIKEY,
-
-        keyword: inputSearch.value || ''
-
-        })
-
-    try {
-        const response = await fetch(URL + param.toString())
-        const posts = await response.json()
-        console.log(posts);
-
-
-        const events =  posts
-               
-        const markup =  events._embedded.events.map(event => {
-            const eventparams = {
-            id: event.id,
-            img:event.images[0].url,
-            date:event.dates.start.localDate,
-            name:event.name,
-            location:event._embedded.venues[0].name
-            }
-            return cardstemplate(eventparams)
-        })
-                
-        cardsList.innerHTML = markup.join("")
-        
-    }
-    catch (err) {
-        console.log(err)
-    }
-    
-}
-
 
 dropdown.addEventListener('click', (e) => {
     chooseCountry(e)
-    getCardsByCountry()
-
-    console.log(currentCountry)
-
+    if (currentCountry) {
+        getEvents()
+    }
 })
 
 inputSearch.addEventListener('input', _.throttle(() => {
-    getCardsByKeyword()
-
+    getEvents()
 }, 1000))
+
+window.addEventListener('DOMContentLoaded', () => {
+    getEvents()
+})
