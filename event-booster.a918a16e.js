8194342c985c37c3432329441e71ce08b14c5039
Yulchikss <yuliapelia19@gmail.com>
2025-03-19 15:51:11 +0200

    Updates

---
+++ event-booster.a918a16e.js
@@ -0,0 +1,2 @@
+let dropdown=document.getElementById("js-dropdown"),dropdownWrapper=document.getElementById("js-dropdownWrapper");dropdownWrapper.addEventListener("click",()=>{dropdown.classList.toggle("is-show")});
+//# sourceMappingURL=event-booster.a918a16e.js.map
